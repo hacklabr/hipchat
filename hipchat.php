144557679be8c5d7a9c680b429a2bf5879bbaffe
leogermani <leogermani@gmail.com>
2014-08-15 10:38:15 -0300

    Add an option to send notification for new comments

--- hipchat.php
+++ hipchat.php
@@ -12,30 +12,56 @@ License: GNU GPL v2
 include_once('lib/HipChat.php');
 
 function hipchat_send_publish_notification($post) {
-  try {
-    $auth_token = get_option('hipchat_auth_token');
-
-    // do nothing if plugin is not configured
-    if (!$auth_token) {
-      return;
-    }
-
-    $room = get_option('hipchat_room');
-    $from = get_option('hipchat_from');
-
+  
     $message = get_userdata($post->post_author)->display_name.
                " just posted <a href='".get_permalink($post->ID)."'>".
                $post->post_title."</a>";
-    $hc = new HipChat($auth_token);
-    $r = $hc->message_room($room, $from, $message);
-    if (!$r) {
-      // something went wrong! what do we do with an error in WP?
-    }
-  } catch (HipChat_Exception $e) {
-    // something went wrong! what do we do with an error in WP?
-  }
+    
+    hipchat_send_notification($message);
+    
+    return $post;
+}
 
-  return $post;
+function hipchat_send_comment_notification($comment_ID, $comment_data) {
+
+    
+    $comment = get_comment($comment_ID);
+    $comment_link = get_comment_link($comment_ID);
+    $post_link = get_permalink($comment->comment_post_ID);
+    $post_title = get_the_title($comment->comment_post_ID);
+    
+    $message = get_userdata($comment->user_id)->display_name . 
+            " just <a href='$comment_link'>replied</a> to <a href='$post_link'>$post_title</a>";
+    
+    if (get_option('hipchat_notify_comments_content'))
+        $message .=  '<br/>' . $comment->comment_content;
+        
+    $auth_token = get_option('hipchat_auth_token');
+    
+    hipchat_send_notification($message);
+    
+}
+
+function hipchat_send_notification($message) {
+    
+    try {
+        $auth_token = get_option('hipchat_auth_token');
+        // do nothing if plugin is not configured
+        if (!$auth_token) {
+          return;
+        }
+
+        $room = get_option('hipchat_room');
+        $from = get_option('hipchat_from');
+
+        $hc = new HipChat($auth_token);
+        $r = $hc->message_room($room, $from, $message);
+        if (!$r) {
+          // something went wrong! what do we do with an error in WP?
+        }
+    } catch (HipChat_Exception $e) {
+        // something went wrong! what do we do with an error in WP?
+    }
 }
 
 function hipchat_menu() {
@@ -62,6 +88,8 @@ function hipchat_settings_page() {
     $auth_token = trim($_POST['auth_token']);
     $from = trim($_POST['from']);
     $room = trim($_POST['room']);
+    $comments = $_POST['comments'];
+    $comments_content = $_POST['comments_content'];
 
     // make sure token is valid and room exists
     $hc = new HipChat($auth_token);
@@ -87,12 +115,16 @@ function hipchat_settings_page() {
       update_option('hipchat_auth_token', $auth_token);
       update_option('hipchat_from', $from);
       update_option('hipchat_room', $room);
+      update_option('hipchat_notify_comments', $comments ? true : false);
+      update_option('hipchat_notify_comments_content', $comments_content ? true : false);
       $updated = 'Settings saved! Auth token is valid and room exists.';
     }
   } else {
     $auth_token = get_option('hipchat_auth_token');
     $from = get_option('hipchat_from');
     $room = get_option('hipchat_room');
+    $comments = get_option('hipchat_notify_comments');
+    $comments_content = get_option('hipchat_notify_comments_content');
 
     if (!$from) {
       $from = 'WordPress';
@@ -106,6 +138,8 @@ function hipchat_setup_options() {
   add_option('hipchat_auth_token');
   add_option('hipchat_from', 'WordPress');
   add_option('hipchat_room');
+  add_option('hipchat_notify_comments');
+  add_option('hipchat_notify_comments_content');
 }
 
 // Attach stuff
@@ -115,6 +149,10 @@ add_action('pending_to_publish', 'hipchat_send_publish_notification');
 add_action('new_to_publish', 'hipchat_send_publish_notification');
 add_action('future_to_publish', 'hipchat_send_publish_notification');
 add_filter('plugin_action_links', 'hipchat_plugin_action_links', 10, 2);
+
+if (get_option('hipchat_notify_comments'))
+    add_action('comment_post', 'hipchat_send_comment_notification', 10, 2);
+
 register_activation_hook(__FILE__, 'hipchat_setup_options');
 
 ?>
